8ce42814e94cc97f1a7fe254b1b93d60a00f1dc8
Domantas <domantas.mauruca@gmail.com>
2019-06-01 13:13:33 +0200

    added Table component and a simple UI to show a list of APIs

---
+++ components/Table.ts
@@ -0,0 +1,59 @@
+import { htm as html } from "@zeit/integration-utils";
+
+export const Table = ({
+  header,
+  children
+}: {
+  header: any;
+  children: any;
+}) => html`
+  <Box
+    width="100%"
+    display="table"
+    borderRadius="4px"
+    borderColor="#eaeaea"
+    overflow="hidden"
+    borderStyle="solid"
+    borderWidth="1px"
+    marginTop="20px"
+    marginBottom="20px"
+  >
+    <Box display="table-header-group" backgroundColor="#fff">
+      <Box display="table-row">
+        ${header}
+      </Box>
+    </Box>
+    <Box display="table-row-group">
+      ${children}
+    </Box>
+  </Box>
+`;
+
+export const TableRow = ({ children }: { children: any }) => html`
+  <Box
+    display="table-row"
+    borderColor="#eaeaea"
+    borderStyle="solid"
+    borderBottomWidth="1px"
+    backgroundColor="white"
+  >
+    ${children}
+  </Box>
+`;
+
+export const HeaderItem = ({ children }: { children: any }) => html`
+  <Box
+    display="table-cell"
+    padding="10px"
+    borderColor="#eaeaea"
+    borderStyle="solid"
+    borderWidth="0px"
+    borderBottomWidth="1px"
+  >
+    <Box fontWeight="400" color="#666666" fontSize="12px" >${children}</Box>
+  </Box>
+`;
+
+export const BodyItem = ({ children }: { children: any }) => html`
+  <Box display="table-cell" padding="10px">${children}</Box>
+`;
--- views/list.ts
+++ views/list.ts
@@ -1,9 +1,27 @@
 import { HandlerOptions, htm } from "@zeit/integration-utils";
+import { Table, HeaderItem } from "../components/Table";
 
 export const getListView = async ({ payload }: HandlerOptions) => {
   return htm`
     <Box>
-        Hello world
+      <Box display="flex" justifyContent="center" marginBottom="14px">
+        <Box textAlign="center" maxWidth="550px">
+          <Box fontSize="24px" fontWeight="400" marginBottom="10px" marginTop="20px">Endpoints</Box>
+          <Box lineHeight="24px">
+            Create GraphQL endpoint within any of the supported providers with a couple of clicks. All required information to use
+            the endpoint will be exposed to all your projects.
+          </Box>
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="flex-end">
+        <Button small>Create</Button>
+      </Box>
+        <${Table} header=${htm`
+            <${HeaderItem}>NAME</${HeaderItem}>
+            <${HeaderItem}>HOSTED AT</${HeaderItem}>
+        `}>
+
+        </${Table}>
     </Box>
   `;
 };
