b9354e2ba84e5b63ebbfd9d8e57a17711ba28388
Lukas <theluksys3@gmail.com>
2019-06-01 19:13:48 +0300

    added create new endpoint basic UI

--- views/getPageContent.ts
+++ views/getPageContent.ts
@@ -1,6 +1,7 @@
 import { HandlerOptions } from "@zeit/integration-utils";
 import { getListView } from "./list";
 import { getSetupInitialView } from "./setup/initial";
+import { getSetupNewView } from "./setup/new-endpoint";
 import { getSetupExistingView } from "./setup/existing-endpoint";
 
 export const getPageContent = async (options: HandlerOptions) => {
@@ -8,6 +9,10 @@ export const getPageContent = async (options: HandlerOptions) => {
   switch(options.payload.action){
     case 'setup-initial':
       return await getSetupInitialView();
+    case 'setup-new-endpoint':
+        return await getSetupNewView(options);
+    case 'setup-new-endpoint-submit':
+        return await getSetupNewView(options, true);
     case 'setup-existing-endpoint':
       return await getSetupExistingView(options);
     case 'setup-existing-endpoint-submit':
